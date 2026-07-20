c6e8ce681be8715065b27f35290bf74277929c51
Daniil Zhdanov <xm8z7pr9hb@privaterelay.appleid.com>
2026-07-20 15:25:45 +0200

    feat: add CLI UI menu

--- AuroCI.CLI/Program.cs
+++ AuroCI.CLI/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Spectre.Console;
+
+// Here it clears the screen
+AnsiConsole.Clear();
+
+// Here it makes a big ASCII-logo
+AnsiConsole.Write(new FigletText("AuroCI")
+    .LeftJustified()
+    .Color(Color.SpringGreen3));
+
+// Make some text info about system success and status
+AnsiConsole.Markup("[bold white]Hello it's AuroCI[/] - your tool to automate CI/CD pipelines and manage your projects with ease.");
+AnsiConsole.Markup("System status: [bold green]OK[/] - All systems are operational.\n");
+
+// Making new rule to exit 
+var rule = new Rule("[bold grey]press any key to exit[/]");
+rule.Justification = Justify.Left;
+AnsiConsole.Write(rule);
+
+Console.ReadKey();
